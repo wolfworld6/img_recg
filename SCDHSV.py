c265285889ff6abdd5d74683b440305d508bca24
Administrator <1617225808@qq.com>
2018-08-09 17:36:32 +0800

    先对图片进行聚类

--- SCDHSV.py
+++ SCDHSV.py
@@ -4,11 +4,14 @@ from PIL import Image
 import cv2
 import numpy as np
 import os
+from sklearn.cluster import KMeans
 '''
    @author:bqFirst
    date:2018-8
 
 '''
+
+
 # Compute low order moments(1,2,3)
 def color_moments(img):
     """compute feature with color moment"""
@@ -38,6 +41,25 @@ def color_moments(img):
     s_thirdMoment = s_skewness**(1./3)
     v_thirdMoment = v_skewness**(1./3)
     color_feature.extend([h_thirdMoment, s_thirdMoment, v_thirdMoment])
+    return color_feature
+
+
+def color_mean(img):
+    """compute feature with color moment"""
+
+    # Convert BGR to HSV color space
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    # Split the channels - h,s,v
+    h, s, v = cv2.split(hsv)
+    # Initialize the color feature
+    color_feature = []
+    # N = h.shape[0] * h.shape[1]
+    # The first central moment - average
+    h_mean = np.mean(h)  # np.sum(h)/float(N)
+    s_mean = np.mean(s)  # np.sum(s)/float(N)
+    v_mean = np.mean(v)  # np.sum(v)/float(N)
+    color_feature.extend([h_mean, s_mean, v_mean])
+
     return color_feature
 
 
@@ -99,28 +121,28 @@ def abnorm_discriminant(filename, test, model, threshold):
         sum = sum + np.linalg.norm(test[i]-model[i])
 
     error = sum / len(test)
-    print(error, end=':')
+    # print(error, end=':')
 
     # error < 45 说明为正常图片
     if error < threshold:
         if filename == 'normal':
-            print("判断为正常: 判断正确")
+            # print("判断为正常: 判断正确")
             flag = 1
             return flag
         else:
-            print("判断为正常: 判断错误")
+            # print("判断为正常: 判断错误")
             return flag
     else:
         if filename == 'abnormal':
-            print("判断为异常: 判断正确")
+            # print("判断为异常: 判断正确")
             flag = 1
             return flag
         else:
-            print("判断为异常: 判断错误")
+            # print("判断为异常: 判断错误")
             return flag
 
 
-def train_threshold(imgs_cf, model):
+def get_mid(imgs_cf, model):
     """
     :param imgs_cf:正常图片特征:[块数[图片数[特征维度]]]
     :param model: 由正常图片特征合成的块特征
@@ -133,9 +155,80 @@ def train_threshold(imgs_cf, model):
             x = np.linalg.norm(np.array(f[i]) - model[i])
             sum = sum+x
         dists.append(sum/len(model))
-    threshold = 1.5*dists[int(len(dists)/2)]
-    print("threshold: ", threshold)
-    return threshold
+    mid = dists[int(len(dists)/2)]
+    # print("threshold: ", threshold)
+    return mid
+
+
+def image_cluster(data, k):
+    """
+    :param data:100 * 108（12x9）
+    :param k: 我们将图片分成三类，大概为：拥堵，正常，空旷
+    :return: 类别索引
+    """
+    estimator = KMeans(n_clusters=k,init='k-means++')
+    estimator.fit(data)
+    label_pred = estimator.labels_
+    index1 = []
+    index2 = []
+    index3 = []
+    for l in range(len(label_pred)):
+        if label_pred[l] == 0:
+            index1.append(l)
+        elif label_pred[l] == 1:
+            index2.append(l)
+        else:
+            index3.append(l)
+
+    centroids = estimator.cluster_centers_
+    # print("标签：\n", label_pred)
+    # print("聚类中心：\n",centroids)
+    return index1, index2, index3
+
+
+def features(label, vx, vy, n_blk):
+    """
+
+    :param label: 聚类类别对应的图片索引
+    :param vx: 图像块的宽
+    :param vy: 图像块的长
+    :param n_blk: 图像切成 n_blk 块
+    :return: 按块特征的平均值，每张图像的特征
+    """
+    # compute features of images
+    blocks_featrue = []
+    imgs_feature = []
+    # 将图像分块
+    for i in range(n_blk):
+        blocks_featrue.append([])
+    for l in label:
+        img_f = []
+        res = cut(files_norm[l], vx, vy)
+        for b in range(len(res)):
+            blk_f = color_moments(res[b])
+            blocks_featrue[b].append(blk_f)
+            img_f.append(blk_f)
+
+        imgs_feature.append(img_f)
+
+    # 计算每个图像块特征的均值、标准差（所有normal图片）
+    i = 0
+    model = []  # 将正常图片的颜色矩特征的均值作为 model
+    for b in blocks_featrue:
+        # b：12 x 9 图片数量 x 特征维度
+        i = i + 1
+        arr = np.array(b)
+        rows, cols = arr.shape
+        # 每个块每一特征维度均值
+        mean = np.mean(arr, 0)
+        model.append(mean)
+        # 每一特征维度标准差
+        std = np.std(arr, 0)
+        std_2 = np.linalg.norm(std)
+        # print("第 %d 个块： " % i)
+        # print("平均值： ", mean)
+        # print("标准差: ", std_2)
+    return model, imgs_feature
 
 
 if __name__ == "__main__":
@@ -146,66 +239,47 @@ if __name__ == "__main__":
     for root, dirs, files_name in os.walk(file_dir_norm):
         for file in files_name:
             files_norm.append(root + '\\' + file)
-    # compute features of normal images
-    f_norm = []
-    blocks = []
-    imgs_cf_norm = []
-    # 将图像分块
-    vx = 1008
-    vy = 1008
-    n_blk = 4*3
-    for b in range(n_blk):
-        blocks.append([])
-    for file in files_norm:
-        img_cf_norm = []
-        res = cut(file, vx, vy)
-        for b in range(len(res)):
-            blk_cf = color_moments(res[b])
-            blocks[b].append(blk_cf)
-            img_cf_norm.append(blk_cf)
 
-        imgs_cf_norm.append(img_cf_norm)
+    # 提取整张图片特征，用于聚类
+    feature = []
+    for f in files_norm:
+        img = cv2.imread(f)
+        ft = color_moments(img)
+        feature.append(ft)
+    idx1, idx2, idx3 = image_cluster(feature, 3)
 
-    # 计算每个图像块特征的均值、标准差（所有normal图片）
-    i = 0
-    cf_mean_model = []  # 将正常图片的颜色矩特征的均值作为 model
-    for b in blocks:
-        # b：12 x 9 图片数量 x 特征维度
-        i = i+1
-        arr = np.array(b)
-        rows, cols = arr.shape
-        # 每个块每一特征维度均值
-        mean = np.mean(arr, 0)
-        cf_mean_model.append(mean)
-        # 每一特征维度标准差
-        std = np.std(arr, 0)
-        std_2 = np.linalg.norm(std)
-        # print("第 %d 个块： " % i)
-        # print("平均值： ", mean)
-        # print("标准差: ", std_2)
+    # 计算各个聚类的model特征，图像特征
+    vx = 1008  # 块宽
+    vy = 1008  # 块长
+    n_blk = 4*3  # 切分成 n_blk 个块
 
-    # 通过正常图片特征与model差值得到阈值
-    threh = train_threshold(imgs_cf_norm, cf_mean_model)
+    model1, imgs_feature1 = features(idx1, vx, vy, n_blk)
+    model2, imgs_feature2 = features(idx2, vx, vy, n_blk)
+    model3, imgs_feature3 = features(idx3, vx, vy, n_blk)
+    # 通过正常图片特征与model的欧氏距离得到差值中位数
+    mid1 = get_mid(imgs_feature1, model1)
+    mid2 = get_mid(imgs_feature2, model2)
+    mid3 = get_mid(imgs_feature3, model3)
     # 根据标准差，得出比较活跃的块，并将这些块去掉
 
     # 计算测试图片的块特征，并于正常图片比对，然后进行异常判别
-    file_dir_test = "E:\GitRepository\img_recg\data\shipai\\test"
-    # compute features of test images
-    num_corr = 0
+    file_dir_test = "E:\GitRepository\img_recg\data\shipai\\abnormal"
+
+    # test image abnormal detection
+    count = 0
     for root, dirs, files_name in os.walk(file_dir_test):
-        for file in files_name:
-            file_dir = root + '\\' + file
-            img_cf = []
-            res = cut(file_dir, vx, vy)
+        for f in files_name:
+            file_test = root + "\\" + f
+            res = cut(file_test, vx, vy)
+            img_feat = []
             for b in range(len(res)):
-                blk_cf = color_moments(res[b])
-                arr = np.array(blk_cf)
-                img_cf.append(arr)
+                blk_feat = color_moments(res[b])
+                arr = np.array(blk_feat)
+                img_feat.append(arr)
+            flag1 = abnorm_discriminant(f.split('_')[0], img_feat, model1, 1.5*mid1)
+            flag2 = abnorm_discriminant(f.split('_')[0], img_feat, model2, 1.5*mid2)
+            flag3 = abnorm_discriminant(f.split('_')[0], img_feat, model3, 1.5*mid3)
+            if flag1 or flag2 or flag3:
+                count = count + 1
 
-            print(file, end=': ')
-            flag = abnorm_discriminant(file.split('_')[0], img_cf, cf_mean_model, threh)
-            num_corr = num_corr + flag
-
-    print("测试图片总数：%d, 正确判断总数：%d, 正确率为：%d" % (len(files_name), num_corr, num_corr/len(files_name)))
-
-
+    print("测试图片总数：%d, 正确判断总数：%d, 正确率为：%f" % (len(files_name), count, count / len(files_name)))
