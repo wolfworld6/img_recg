407881989a5abbf79f67275bdd4e9f89dea262be
mel-on <melon_o@163.com>
2018-08-15 08:15:04 +0800

    基于KNN的异常图片识别

---
+++ pHashModel/classify_knn.py
@@ -0,0 +1,113 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/8/10 16:11
+# @Author  : Lijianfeng
+# @Project : img_recg
+
+from pHashModel.pHash import *
+from pHashModel.phash_config import *
+import os
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+img_labels = []
+
+
+def df_clu_img(clusterd_img_dir):
+    """
+    把clusterd中的图片文件转换成指纹
+    :param img_dir:
+    :return:
+    """
+    # 读取聚类后的图片
+    for root, dirs, files in os.walk(clusterd_img_dir):
+        # cluster_name = root.split("\\")[-1]
+        for name in files:
+            path = os.path.join(root, name)
+            avh = avhash(path)
+            img_labels.append([name, avh, 'nor'])
+
+    return img_labels
+
+
+def df_abn_img(abnormal_img_dir):
+    """
+    异常图片转换成指纹
+    :param abnormal_img_dir:
+    :return:
+    """
+    os.chdir(abnormal_img_dir)
+    images = []
+    images.extend(glob.glob('*.JPG'))
+    for i in images:
+        img_labels.append([i, avhash(i), 'abn'])
+
+    return img_labels
+
+
+def init_img():
+    """
+    将图片转换成指纹，并打上标签，保存到  knn_data_set.csv 中
+    :return:
+    """
+    df_clu_img(clusterd_img_dir)
+    df_abn_img(abnormal_img_dir)
+    os.chdir(sys.path[0])
+    pd.DataFrame(img_labels, columns=["img", "fingerprint", "label"]).to_csv(knn_data_set)
+
+
+def split_train_test(df):
+    X_train, X_test, y_train, y_test = train_test_split(df, df, test_size=0.3, random_state=0)
+    return X_train, X_test
+
+
+def knn(k, train, test):
+    """
+    实现 knn 算法
+    :param k:
+    :param train:
+    :param test:
+    :return:
+    """
+    result = []
+    for test_index, test_row in test.iterrows():
+        h = []
+        for train_index, train_row in train.iterrows():
+            h.append([test_row['img'], train_row['img'], hamming(test_row['fingerprint'], train_row['fingerprint']), test_row['label'], train_row['label']])
+        dfh = pd.DataFrame(h, columns=['test_img', 'train_img', 'hamming', 'test_label', 'train_label'])
+        dfh = dfh.sort_values(by='hamming')
+        dfh = dfh[:k]
+        predict_label = dfh['train_label'].value_counts().idxmax()
+        result.append([test_row['img'], test_row['label'], predict_label])
+    return pd.DataFrame(result, columns=['img', 'original_label', 'predict_label'])   # 预测结果
+
+
+def summary(predict_result):
+    tp = predict_result[(predict_result['original_label'] == 'abn') & (predict_result['predict_label'] == 'abn')].iloc[:, 0].size
+    fn = predict_result[(predict_result['original_label'] == 'abn') & (predict_result['predict_label'] == 'nor')].iloc[:, 0].size
+    fp = predict_result[(predict_result['original_label'] == 'nor') & (predict_result['predict_label'] == 'abn')].iloc[:, 0].size
+    tn = predict_result[(predict_result['original_label'] == 'nor') & (predict_result['predict_label'] == 'nor')].iloc[:, 0].size
+
+    a = (tp + tn) / (tp + fn + fp + tn)     # 正确率accuracy：正确预测的
+    p = tp / (tp + fp)  # 精确率precision：预测为异常图片中多少正确的
+    r = tp / (tp + fn)  # 召回率recall：总共的异常图片中多少被正确预测为异常的
+    f1 = 2 * (p * r/(p + r))    # F1-measure：精确值和召回率的调和均值，精确率和召回率都高时，F1值也会高
+    return a, p, r, f1
+
+
+def main():
+    # 初始化数据时执行 init_img()
+    # init_img()
+    df = pd.read_csv(knn_data_set)
+    train, test = split_train_test(df)
+    predict_result = knn(10, train, test)
+    a, p, r, f1 = summary(predict_result)
+    row = pd.DataFrame([['accuracy', '', a], ['precision', '', p], ['recall', '', r], ['F1-measure', '', f1]], columns=['img', 'img,original_label', 'predict_label'])
+    predict_result = predict_result.append(row, ignore_index=True)
+    os.chdir(sys.path[0])
+    predict_result.to_csv('../data/shipai/knn_result.csv')
+    print(predict_result)
+    print('accuracy: ', a, 'precision: ', p, '\t', 'recall', r, '\t', 'F1-measure:', f1)
+
+
+if __name__ == '__main__':
+    main()
---
+++ pHashModel/phash_config.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time    : 2018/8/13 11:33
+# @Author  : Lijianfeng
+# @Project : img_recg
+
+normal_img_dir = '../data/shipai/original/normal'       # 正常图片
+abnormal_img_dir = '../data/shipai/original/abnormal'   # 异常图片
+clusterd_img_dir = '../data/shipai/clusterd'            # 聚类后的图片
+knn_data_set = '../data/shipai/knn_data_set.csv'        # knn分类前处理好的数据
+# normal_img_dir = 'E:/bigdata/pycharmProject/img_recg/data/shipai/original/normal'       # 正常图片
+# abnormal_img_dir = 'E:/bigdata/pycharmProject/img_recg/data/shipai/original/abnormal'   # 异常图片
+# clusterd_img_dir = 'E:/bigdata/pycharmProject/img_recg/data/shipai/clusterd'                     # 聚类后的图片
